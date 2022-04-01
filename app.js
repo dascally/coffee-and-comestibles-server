23a9b7d47f97e5547cc136d6390e54aec40929de
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-04-01 19:02:07 -0400

    Move dev-only dep morgan to dynamic import so it doesn't import in prod

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import morgan from 'morgan';
 import events from './routes/events.js';
 import menu from './routes/menu.js';
 import order from './routes/order.js';
 import users from './routes/users.js';
 
 const app = express();
+
 if (process.env.NODE_ENV === 'development') {
+  const { default: morgan } = await import('morgan');
   app.use(morgan('dev'));
 }
 
