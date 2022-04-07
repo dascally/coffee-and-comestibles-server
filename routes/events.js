6912b1026c559a0568a4715b959dfb3cdd91fbe0
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-04-07 15:20:50 -0400

    feat: Implement the /events route

--- routes/events.js
+++ routes/events.js
@@ -1,26 +1,49 @@
 import express from 'express';
+import { userExtractor, verifyAdmin } from '../utils/middleware.js';
+import Event from '../models/event.js';
 const router = express.Router();
 
 router
   .route('/')
+  .get(async (req, res, next) => {
+    try {
+      const events = await Event.find({});
+
+      return res.status(200).json(events);
+    } catch (err) {
+      return next(err);
+    }
+  })
+  .post(userExtractor, verifyAdmin, async (req, res, next) => {
+    try {
+      const newEvent = new Event({
+        title: req.body.title,
+        datetime: new Date(req.body.datetime),
+        description: req.body.description,
+        image: req.body.image || undefined,
+      });
+
+      const savedEvent = await newEvent.save();
+
+      return res
+        .status(201)
+        .set('Location', `/${savedEvent._id}`)
+        .json(savedEvent);
+    } catch (err) {
+      return next(err);
+    }
+  })
   .all((req, res, next) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(`Received a ${req.method} request for ${req.url}.\n`);
-    next();
-  })
-  .get((req, res) => {
-    res.end(
-      'Got a GET request for /events. This will return JSON for all events.'
-    );
-  })
-  .post((req, res) => {
-    res.end('This will be used to add an event. (POST)');
-  })
-  .put((req, res) => {
-    res.end('PUT not supported');
-  })
-  .delete((req, res) => {
-    res.end('DELETE not supported');
+    try {
+      return res
+        .status(405)
+        .set('Allow', 'GET, POST')
+        .json({
+          message: `${req.method} is not supported on the /events path.`,
+        });
+    } catch (err) {
+      return next(err);
+    }
   });
 
 router
